0f225b32c3797026156fdcde81a3991ebf29a465
milenatarkowska <milenatarkowska1@gmail.com>
2026-03-15 11:37:30 +0100

    Revert "Deleted Id field in User"
    This reverts commit ae59f87314e816e42e46ffaf5a870c28b79ab7ee.

--- MP1_APBD_s30638/Users/Użytkownik.cs
+++ MP1_APBD_s30638/Users/Użytkownik.cs
@@ -4,10 +4,12 @@ public abstract class Użytkownik
 {
    protected string Imie { get; set; }
    protected string Nazwisko { get; set; }
+   protected string Id {get; set;}
    protected TypUżytkownika Typ { get; set; }
 
    protected Użytkownik(string imie, string nazwisko, TypUżytkownika typ)
        {
+           Id = Guid.NewGuid().ToString();
            Imie = imie;
            Nazwisko = nazwisko;
            Typ = typ;
