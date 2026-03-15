a2a995768477b7780af9601dbc8549e067ea28a2
milenatarkowska <milenatarkowska1@gmail.com>
2026-03-15 11:39:56 +0100

    Added Pracownik class

---
+++ MP1_APBD_s30638/Users/Pracownik.cs
@@ -0,0 +1,11 @@
+namespace MP1_APBD_s30638.Users;
+
+public class Pracownik : Użytkownik
+{
+    private string Stanowisko { get; set; }
+
+    public Pracownik(string imie, string nazwisko, TypUżytkownika typ, string stanowisko) : base(imie, nazwisko, typ)
+    {
+        Stanowisko = stanowisko;
+    }
+}
