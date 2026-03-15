ae59f87314e816e42e46ffaf5a870c28b79ab7ee
milenatarkowska <milenatarkowska1@gmail.com>
2026-03-15 11:30:17 +0100

    Deleted Id field in User

--- MP1_APBD_s30638/Users/Użytkownik.cs
+++ MP1_APBD_s30638/Users/Użytkownik.cs
@@ -4,12 +4,10 @@ public abstract class Użytkownik
 {
    protected string Imie { get; set; }
    protected string Nazwisko { get; set; }
-   protected string Id {get; set;}
    protected TypUżytkownika Typ { get; set; }
 
    protected Użytkownik(string imie, string nazwisko, TypUżytkownika typ)
        {
-           Id = Guid.NewGuid().ToString();
            Imie = imie;
            Nazwisko = nazwisko;
            Typ = typ;
