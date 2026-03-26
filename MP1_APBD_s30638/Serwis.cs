7bd18f79bdb1c4a624980f952b5828c36f586dbe
milenatarkowska <milenatarkowska1@gmail.com>
2026-03-26 12:42:08 +0100

    Moved Zwrot to Serwis from Wypożyczenie

--- MP1_APBD_s30638/Serwis.cs
+++ MP1_APBD_s30638/Serwis.cs
@@ -6,7 +6,7 @@ public class Serwis
 {
     private List<Użytkownik> _użytkownicy = new List<Użytkownik>();
     private List<Sprzęt> _sprzęt = new List<Sprzęt>();
-    private List<Wypożyczenie> _wypożyczenie = new List<Wypożyczenie>();
+    private List<Wypożyczenie> _wypożyczenia = new List<Wypożyczenie>();
 
     public void DodajUżytkownika(Użytkownik użytkownik)
     {
@@ -27,6 +27,7 @@ public class Serwis
 
     public void WyświetlCałySprzęt()
     {
+        Console.WriteLine("Lista całego sprzętu");
         foreach (var s in _sprzęt)
         {
             Console.WriteLine($"{s.Nazwa} - Status: {s.Status}");
@@ -36,10 +37,72 @@ public class Serwis
     public void WyświetlDostępne()
     {
         var dostepne = _sprzęt.Where(s => s.Status == StatusSprzętu.Dostępny);
-    
+        
+        Console.WriteLine("Lista dostępnego sprzętu");
+        
         foreach (var s in dostepne)
         {
             Console.WriteLine($"Wolny sprzęt: {s.Nazwa}");
         }
+    }
+
+    public void Wypożycz(Sprzęt sprzęt, Użytkownik użytkownik)
+    {
+        int aktywneWypożyczenia = 0;
+        
+        foreach (var w in _wypożyczenia)
+            if (w.Użytkownik == użytkownik && w.FaktycznyZwrot == null) aktywneWypożyczenia++;
+        
+        int limit =(użytkownik is Student) ? 2 : 5;
+        
+        if (aktywneWypożyczenia >= limit) {
+            Console.WriteLine($"BŁĄD: {użytkownik.Nazwisko} osiągnął limit {limit} wypożyczeń.");
+            return;
+        }
+
+        if (sprzęt.Status != StatusSprzętu.Dostępny) {
+            Console.WriteLine($"BŁĄD: Sprzęt {sprzęt.Nazwa} jest {sprzęt.Status}.");
+            return;
+        }
+        
+        var wypożyczenie = new Wypożyczenie(użytkownik, sprzęt);
+        _wypożyczenia.Add(wypożyczenie);
+    }
+    
+    public void Zwróć(Sprzęt sprzet)
+    {
+        Wypożyczenie aktywneWypozyczenie = null;
+        
+        foreach (var w in _wypożyczenia)
+        {
+            if (w.Sprzęt == sprzet && w.FaktycznyZwrot == null)
+            {
+                aktywneWypozyczenie = w;
+                break;
+            }
+        }
+        
+        if (aktywneWypozyczenie == null)
+        {
+            Console.WriteLine("BŁĄD: Nie znaleziono aktywnego wypożyczenia dla tego urządzenia.");
+            return;
+        }
+        
+        aktywneWypozyczenie.FaktycznyZwrot = DateTime.Now;
+
+        if (aktywneWypozyczenie.FaktycznyZwrot > aktywneWypozyczenie.TerminZwrotu)
+        {
+            TimeSpan opóźnienie = aktywneWypozyczenie.FaktycznyZwrot.Value - aktywneWypozyczenie.TerminZwrotu;
+            int dni = opóźnienie.Days;
+            
+            double kara = dni * 5;
+            Console.WriteLine($"Zwrot OPÓŹNIONY o {dni} dni. Naliczona kara: {kara}zł");
+        }
+        else
+        {
+            Console.WriteLine("Zwrot w terminie. Brak dodatkowych opłat.");
+        }
+        
+        sprzet.UstawJakoDostępny();
     }
 }
